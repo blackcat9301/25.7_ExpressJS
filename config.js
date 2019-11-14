f4495e9b5ccfc4352198bcfac052e2cce66b3d05
Agnieszka Kot <agnieszkakot22@gmail.com>
2019-11-14 19:38:17 +0100

    google auth

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '107741833343-gcqbsivte6mr04sfoa36vbgt3661ejl0.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'UONwi1IEFhV7cP4126V9FJ7s',
+    'CALLBACK_URL': 'https://localhost:3000/auth/google/callback'
+};
